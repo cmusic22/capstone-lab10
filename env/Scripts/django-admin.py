722c3b00b50f60de48895f32136510f4b5ec8daf
Flounder <testy@test.com>
2020-04-18 14:55:22 -0500

    Through slide 70

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\carson\documents\school\itec 2905-60 software development capstone\topic10\wishlist_with_uploads\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
